bf10d33dcba283d537a0155778a42d640bab2789
Jamaine Tuazon <tuazonjamaine12@gmail.com>
2025-05-11 20:04:43 +0800

    updated index.js from rishaye-backend

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import { dirname } from "path";
 import path from "path";
 import { fileURLToPath } from "url";
 import { pool, sql } from "./db-connection.js";
+import multer from 'multer'
+import bodyParser from 'body-parser'
+import fs from 'fs'
+import cookieSession from 'cookie-session'
 
 //potek isahang import lang pala yung pool tsaka sql para magconnect kaines
 
@@ -12,9 +16,14 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const port = 3000;
 
 //middleware setup
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(express.static(path.join(__dirname, "public")));
+const upload = multer({ dest : path.join(__dirname, 'public', 'uploads', 'featureImage') })
+app.use(cookieSession({
+  name: 'session',
+  keys: ['cooKey'], 
+}));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "views", "index.html"));
@@ -42,12 +51,12 @@ app.post("/register", async (req, res) => {
 });
 
 app.post("/login", async (req, res) => {
-    const { login_userName, login_password } = req.body;
+    const { userName, password } = req.body;
 
     try {
         const result = await pool.request()
-            .input('login_userName', sql.VarChar, login_userName)
-            .query('SELECT * FROM userTABLE WHERE username = @login_userName')
+            .input('userName', sql.VarChar, userName)
+            .query('SELECT * FROM userTABLE WHERE username = @userName')
 
         const user = result.recordset[0]; //stores the result to user variable
         
@@ -57,9 +66,15 @@ app.post("/login", async (req, res) => {
             return res.json({ success: false, message: "Incorrect username or password!" });
         }
 
-        if (user.password !== login_password) {
+        if (user.password !== password) {
             return res.json({ success: false, message: "Incorrect username or password!" });
         }
+
+        // cookie store logged-in user credentials
+        req.session.user = {
+            id: user.userID,
+            username: user.username
+        };
         return res.json({ success: true, message: "Login success!" });
 
     }
@@ -69,13 +84,98 @@ app.post("/login", async (req, res) => {
     }
 });
 
-app.get("/events", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "views", "events.html"))
-})
 app.get("/create-events", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "views", "create-events.html"))
 })
 
+app.post("/create-events", async (req, res) => {   
+    if (!req.session.user) {
+        return res.status(401).json({ message: 'Unauthorized'});
+    }
+    
+    const creatorID = req.session.user.id;
+
+    const { base64FeatureImage, imageFileName, imageFileExtension, eventName, 
+            startDateTime, endDateTime, location, description, category, 
+            feedback, requireApproval, capacity, allowWaitlist, lastUpdated} = req.body
+
+    function getBinaryValue(base64FeatureImage) {
+        const matches = base64FeatureImage.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/)
+        const response = {};
+
+        if (matches.length !== 3) {
+            console.log('Invalid input string');
+        }
+
+        response.type = matches[1];
+        response.data = Buffer.from(matches[2], 'base64');
+
+        return response;
+    }
+
+    const binaryFeatureImage = getBinaryValue(base64FeatureImage);
+
+    if (!binaryFeatureImage) {
+        return res.json({ message: 'Invalid image format.' });
+    }
+    
+    let uploadsfeatureImagesFileName = fs.readdirSync(path.join(__dirname, 'public', 'uploads', 'featureImage'))
+    let featureImageFileName = `${imageFileName}.${imageFileExtension}`
+
+    for (let i = 1; i <= uploadsfeatureImagesFileName.length; i++) {
+        if (uploadsfeatureImagesFileName.includes(featureImageFileName)) {
+            featureImageFileName = `${imageFileName} (${i}).${imageFileExtension}`
+        }
+        else {
+            break
+        }
+    }
+    console.log(featureImageFileName)
+
+    const featureImagePath = path.join(__dirname, 'public', 'uploads', 'featureImage', `${featureImageFileName}`);
+
+    fs.writeFile(featureImagePath, binaryFeatureImage.data, (error) => { 
+        if (error) { 
+            console.log("Image Creation Failed: ", error) 
+        }
+    });
+
+    try {
+        await pool.request()
+            .input('creatorID', sql.Int, creatorID)
+            .input('featureImage', sql.VarChar, featureImagePath)
+            .input('eventName', sql.VarChar, eventName)
+            .input('startDateTime', sql.DateTime, startDateTime)
+            .input('endDateTime', sql.DateTime, endDateTime) 
+            .input('location', sql.VarChar, location)
+            .input('description', sql.VarChar, description)
+            .input('category', sql.VarChar, category)
+            .input('feedbackLink', sql.VarChar, feedback)
+            .input('requireApproval', sql.VarChar, requireApproval)
+            .input('capacity', sql.Int, capacity)
+            .input('allowWaitlist', sql.VarChar, allowWaitlist)
+            .input('lastUpdated', sql.DateTime, lastUpdated)
+            .query(`INSERT INTO eventsTable (eventName, description, category, location, startDateTime, 
+                                endDateTime, featureImage, requireApproval, capacity, feedbackLink, 
+                                allowWaitlist, lastUpdated, creatorID) 
+                    SELECT @eventName, @description, @category, @location, @startDateTime, 
+                            @endDateTime, @featureImage, @requireApproval, @capacity, @feedbackLink,
+                            @allowWaitlist, @lastUpdated, u.userID
+                    FROM userTable u
+                    WHERE userID = @creatorID`)
+
+        console.log('Event creation successful')
+        return res.status(201).json({ message: "Event creation successful" })
+    }
+    catch (e) {
+        console.log("Event Creation Failed: ", e)
+        return res.status(500).json({ message: "Event creation failed", error: e })
+    }
+})
+
+app.get("/events", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "views", "events.html"))
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
