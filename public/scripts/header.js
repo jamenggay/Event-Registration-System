3bf3aa3317cf86b0b1bfddcf7744ee3e80e683a3
Jamaine Tuazon <tuazonjamaine12@gmail.com>
2025-06-02 23:30:50 +0800

    saved changes

--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,6 +1,31 @@
 $(document).ready(function() {
-    $("#header").load("/views/running-marginals/header.html", function() {
+    $("#header").load("../views/running-marginals/header.html", function() {
         console.log('Header loaded!');
     });
-});
 
+    async function getUserBasicInfo() {
+        try {
+            const response = await fetch("/basic-profile", {
+                method: "GET",
+                headers: {
+                    'Accept': 'application/json'
+                }
+            });
+
+            if (response.ok) {
+                const result = await response.json();
+                const profilePic = document.getElementById('profile-picture')
+                profilePic.src = result.profilePic
+            } 
+            else {
+                const error = await response.json();
+                console.log("Backend Failed: ", error);
+            }
+        } 
+        catch (e) {
+            console.log("Client Error: ", e);
+        }
+    }
+
+    getUserBasicInfo();
+});
